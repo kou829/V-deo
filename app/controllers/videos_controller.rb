f5907324ff1b84b2dc87d8db97055b6693145730
kou829 <73116796+kou829@users.noreply.github.com>
2020-12-04 19:00:51 +0900

    動画詳細ページ・編集・削除機能の実装

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -16,6 +16,37 @@ class VideosController < ApplicationController
     end
   end
 
+  def show
+    @video = Video.find(params[:id])
+  end
+
+  def edit
+    @video = Video.find(params[:id])
+    unless current_user.id == @video.user.id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    if @video.update(video_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+    
+  def destroy
+    @video = Video.find(params[:id])
+    if current_user.id == @video.user_id
+      @video.destroy
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+
+
   private
   def video_params
     params.require(:video).permit(:title, :overview, :video).merge(user_id: current_user.id)
