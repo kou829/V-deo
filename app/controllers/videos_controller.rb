072eb1abc998e0841366d200136aeb96273193bd
kou829 <73116796+kou829@users.noreply.github.com>
2020-12-04 16:05:49 +0900

    動画投稿・トップページに表示

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,8 +1,24 @@
 class VideosController < ApplicationController
   def index 
+    @videos = Video.order(created_at: :desc)
   end
 
   def new
+    @video = Video.new
   end
   
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def video_params
+    params.require(:video).permit(:title, :overview, :video).merge(user_id: current_user.id)
+  end
+
 end
